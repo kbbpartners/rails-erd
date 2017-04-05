7510ffbea52c30b929394580372e7dc515494692
Sol Irvine <sol@turnstilelive.com>
2017-04-05 16:29:07 +0900

    Added safety guard for null namespace

--- lib/rails_erd/domain/entity.rb
+++ lib/rails_erd/domain/entity.rb
@@ -93,7 +93,7 @@ module RailsERD
       end
 
       def namespace
-        name.scan(/(.*)::.*/).first.first
+        name.scan(/(.*)::.*/).first.try(:first)
       end
 
       def to_s # @private :nodoc:
